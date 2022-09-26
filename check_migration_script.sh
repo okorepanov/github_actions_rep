5146eb51201973f643d0358f5b8ef1c160f1824a
Aleksander Korepanov <rskillkharkv@gmail.com>
2022-09-26 12:41:46 +0300

    Change Timestamp

--- check_migration_script.sh
+++ check_migration_script.sh
@@ -3,15 +3,14 @@
 set -e # exit on first exception
 set -x # enable debug
 
-MINIMUM_AVAILABLE_DATE=$(date -d '-1 month' +'%Y%m%d')
-#MINIMUM_AVAILABLE_DATE=$(date -v -1m +'%Y%m%d')
+MINIMUM_AVAILABLE_DATE=$(date -d '-1 month' +'%Y%m%d') # macos: $(date -v -1m +'%Y%m%d')
 
 for file in "$@"
 do
-  filename=$(basename $file)
-  if (( ${filename:0:8} < $MINIMUM_AVAILABLE_DATE ))
+  timestamp=$(basename $file | cut -c 1-8)
+  if [ $timestamp -lt $MINIMUM_AVAILABLE_DATE ];
   then
-    echo $filename is older than one month!
+    echo $file is older than one month!
     exit 1;
   fi
 done
