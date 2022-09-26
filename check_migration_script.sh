a016a26bf15d2c5ab01cc6fccc9ee4cea544c3ce
Aleksander Korepanov <rskillkharkv@gmail.com>
2022-09-26 11:14:57 +0300

    Add script to check timestamp

--- check_migration_script.sh
+++ check_migration_script.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-set -e
+set -e # exit on first exception
+set -x # enable debug
 
-exit 1
+MINIMUM_AVAILABLE_DATE=$(date -d '-1 month' +'%Y%m') #macos $(date -v -1m +'%Y%m')
+
+for file in "$@"
+do
+  if (( ${file:0:8} < $MINIMUM_AVAILABLE_DATE ))
+  then
+    echo $file is older than one month!
+    exit 1;
+  fi
+done
